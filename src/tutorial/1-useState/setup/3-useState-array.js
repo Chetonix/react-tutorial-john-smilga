e16c01202fd492c8286726325e91a704cd881d7d
Chetonix <chetonix@gmail.com>
2022-11-19 12:49:10 +0530

    add code to the 2-useState-array file

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+
+  const [dataArray, setDataArray] = useState(data);
+
+  const removeItem = id => {
+    let filteredData = dataArray.filter((data)=>data.id!==id) 
+    setDataArray(filteredData)   
+  }
+
+  return (
+    <React.Fragment>
+     {dataArray.map((data) => {
+
+      const {id} = data;
+
+      return ( 
+      <div key={id} className="item">
+        <h4>{data['name']}</h4>
+        <button onClick={()=> removeItem(id)}>remove</button>
+      </div>)
+     })}
+     <button className="btn" onClick={()=>setDataArray([])}>clear items</button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateArray;
