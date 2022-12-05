ed01ab7d965688af49841220f1cf308e2edc1a22
Chetan Harjani <chetanharjani@Chetans-MacBook-Pro.local>
2022-12-05 15:40:29 +0530

    add code to the setup folder of 7-prop-drilling tutorial

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Setup from "./tutorial/6-useReducer/setup/index";
-import Final from "./tutorial/6-useReducer/final/index";
+import React from "react";
+import Setup from "./tutorial/7-prop-drilling/setup/1-prop-drilling";
+import Final from "./tutorial/7-prop-drilling/final/1-prop-drilling";
 
 function App() {
   return (
-    <div className='container'>
+    <div className="container">
       <Setup />
       {/* <Final /> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
