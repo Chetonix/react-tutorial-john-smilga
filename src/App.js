8d99b98437aa1919393bb16659f88eef9fe5d017
Chetan Harjani <chetanharjani@Chetans-MacBook-Pro.local>
2022-12-21 17:31:29 +0530

    add just a final setup for the 10-prop-types folder, setup folder needs to developed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Setup from "./tutorial/9-custom-hooks/setup/1-fetch-example";
-import Final from "./tutorial/9-custom-hooks/final/1-fetch-example";
+import Setup from "./tutorial/10-prop-types/setup/index";
+import Final from "./tutorial/10-prop-types/final/index";
 
 function App() {
   return (
     <div className="container">
-      <Setup />
-      {/* <Final /> */}
+      {/* <Setup /> */}
+      <Final />
     </div>
   );
 }
