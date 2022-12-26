cf7d2c67c8c99f7f894dd3a4f5579df23f084784
Chetan Harjani <chetanharjani@Chetans-MacBook-Pro.local>
2022-12-26 14:00:01 +0530

    add 12-memo-useMemo-useCallback solutions to the repo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Setup from "./tutorial/10-prop-types/setup/index";
-import Final from "./tutorial/10-prop-types/final/index";
+import Setup from "./tutorial/12-memo-useMemo-useCallback/setup/index";
+import Final from "./tutorial/12-memo-useMemo-useCallback/final/index";
 
 function App() {
   return (
     <div className="container">
-      {/* <Setup /> */}
-      <Final />
+      <Setup />
+      {/* <Final /> */}
     </div>
   );
 }
